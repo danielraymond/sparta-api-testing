ce61848033dcaf9cbaf339b4138ce900f9eb77a5
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-12-14 14:42:58 +0000

    changed test descriptions

--- postcodesio/spec/postcodesio_httparty_spec.rb
+++ postcodesio/spec/postcodesio_httparty_spec.rb
@@ -22,15 +22,15 @@ describe 'Tests for a get and post request to a postcode api' do
     expect(@pc.get_result('PH152AF').length).to eq(23)
   end
 
-  it "Postcode should be a string" do
+  it "Single method postcode should be a string" do
     expect(@pc.get_result('PH152AF')['postcode']).to be_kind_of(String)
   end
 
-  it "Quality should be an integer" do
+  it "Single method quality should be an integer" do
     expect(@pc.get_result('PH152AF')['quality']).to be_kind_of(Integer)
   end
 
-  it "Eastings and Northings should have 6 digits or be nil" do
+  it "Single method Eastings and Northings should have 6 digits or be nil" do
     if @pc.get_result('PH152AF')['eastings'].class == Integer
       expect(@pc.get_result('PH152AF')['eastings'].to_s.size).to eq(6)
     else
@@ -43,7 +43,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "Eastings and Northings should be integers or nil" do
+  it "Single method Eastings and Northings should be integers or nil" do
     if @pc.get_result('PH152AF')['eastings'].class == Integer
       expect(@pc.get_result('PH152AF')['eastings']).to be_kind_of(Integer)
     else
@@ -56,11 +56,11 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "Country should be a string" do
+  it "Single method country should be a string" do
     expect(@pc.get_result('PH152AF')['country']).to be_kind_of(String)
   end
 
-  it "nhs_ha should be a string or nil" do
+  it "Signle method nhs_ha should be a string or nil" do
     if @pc.get_result('PH152AF')['nhs_ha'].class == String
       expect(@pc.get_result('PH152AF')['nhs_ha']).to be_kind_of(String)
     else
@@ -68,12 +68,12 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "longitude and latitude should be floats" do
+  it "Single method longitude and latitude should be floats" do
     expect(@pc.get_result('PH152AF')['latitude']).to be_kind_of(Float)
     expect(@pc.get_result('PH152AF')['longitude']).to be_kind_of(Float)
   end
 
-  it "european electoral region should be a string or nil" do
+  it "Single method european electoral region should be a string or nil" do
     if @pc.get_result('PH152AF')['european_electoral_region'].class == String
       expect(@pc.get_result('PH152AF')['european_electoral_region']).to be_kind_of(String)
     else
@@ -81,7 +81,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "primary care trust should be a string or nil" do
+  it "Single method primary care trust should be a string or nil" do
     if @pc.get_result('PH152AF')['primary_care_trust'].class == String
       expect(@pc.get_result('PH152AF')['primary_care_trust']).to be_kind_of(String)
     else
@@ -89,7 +89,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "region should be a string or nil" do
+  it "Single method region should be a string or nil" do
     if @pc.get_result('PH152AF')['region'].class == String
       expect(@pc.get_result('PH152AF')['region']).to be_kind_of(String)
     else
@@ -97,7 +97,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "lsoa should be a string" do
+  it "Single method lsoa should be a string" do
     if @pc.get_result('PH152AF')['lsoa'].class == String
       expect(@pc.get_result('PH152AF')['lsoa']).to be_kind_of(String)
     else
@@ -105,7 +105,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "msoa should be a string" do
+  it "Single method msoa should be a string" do
     if @pc.get_result('PH152AF')['msoa'].class == String
       expect(@pc.get_result('PH152AF')['msoa']).to be_kind_of(String)
     else
@@ -113,15 +113,15 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "incode should be 3 characters" do
+  it "Single method incode should be 3 characters" do
     expect(@pc.get_result('PH152AF')['incode'].to_s.size).to eq(3)
   end
 
-  it "outcode should be 2, 3 or 4 characters" do
+  it "Single method outcode should be 2, 3 or 4 characters" do
     expect(@pc.get_result('PH152AF')['outcode'].size).to be_between(2,4).inclusive
   end
 
-  it "parliamentary constiuency should be a string or nil" do
+  it "Single method parliamentary constiuency should be a string or nil" do
     if @pc.get_result('PH152AF')['parliamentary_constituency'].class == String
       expect(@pc.get_result('PH152AF')['parliamentary_constituency']).to be_kind_of(String)
     else
@@ -129,7 +129,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "admin district should be a string or nil" do
+  it "Single method admin district should be a string or nil" do
     if @pc.get_result('PH152AF')['admin_distric'].class == String
       expect(@pc.get_result('PH152AF')['admin_distric']).to be_kind_of(String)
     else
@@ -137,7 +137,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "parish should be a string or nil" do
+  it "Single method parish should be a string or nil" do
     if @pc.get_result('PH152AF')['parish'].class == String
       expect(@pc.get_result('PH152AF')['parish']).to be_kind_of(String)
     else
@@ -145,7 +145,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "admin county should be a string or nil" do
+  it "Single method admin county should be a string or nil" do
     if @pc.get_result('PH152AF')['admin_county'].class == String
       expect(@pc.get_result('PH152AF')['admin_county']).to be_kind_of(String)
     else
@@ -153,7 +153,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "admin ward should be a string or nil" do
+  it "Single method admin ward should be a string or nil" do
     if @pc.get_result('PH152AF')['admin_ward'].class == String
       expect(@pc.get_result('PH152AF')['admin_ward']).to be_kind_of(String)
     else
@@ -161,7 +161,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "ccg should be a string or nil" do
+  it "Single method ccg should be a string or nil" do
     if @pc.get_result('PH152AF')['ccg'].class == String
       expect(@pc.get_result('PH152AF')['ccg']).to be_kind_of(String)
     else
@@ -169,7 +169,7 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "nuts should be a string or nil" do
+  it "Single method nuts should be a string or nil" do
     if @pc.get_result('PH152AF')['nuts'].class == String
       expect(@pc.get_result('PH152AF')['nuts']).to be_kind_of(String)
     else
@@ -177,11 +177,11 @@ describe 'Tests for a get and post request to a postcode api' do
     end
   end
 
-  it "codes should be in a hash" do
+  it "Single method codes should be in a hash" do
     expect(@pc.get_codes('PH152AF')).to be_kind_of(Hash)
   end
 
-  it "all codes should be a string or nil" do
+  it "Single method codes should be a string or nil" do
     @pc.get_codes('PH152AF').each do |i|
       if i[1].class == String
         expect(i[1]).to be_kind_of(String)
@@ -190,7 +190,5 @@ describe 'Tests for a get and post request to a postcode api' do
       end
     end
   end
-
-
 
 end
