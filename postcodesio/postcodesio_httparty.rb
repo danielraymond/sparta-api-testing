2e21e2840f524fe1ff2d2a8798ebf2584f24d21f
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-12-14 10:08:03 +0000

    created method to create post request

--- postcodesio/postcodesio_httparty.rb
+++ postcodesio/postcodesio_httparty.rb
@@ -10,8 +10,13 @@ class Postcodesio
     JSON.parse(self.class.get("/postcodes/#{postcode}").body)
   end
 
+  def get_multiple_postcodes(postcodes_array)
+    JSON.parse(self.class.post("/postcodes/", body: { "postcodes" => postcodes_array}).body)
+  end
+
 end
 
 x = Postcodesio.new
 
 puts x.get_single_postcode('ha86lh')
+# puts x.get_multiple_postcodes(["ha86lh", "bs85bb", "bs11dq"])
