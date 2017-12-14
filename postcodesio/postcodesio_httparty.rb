7170805de932f171d7363c403e58920d7a0d7533
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-12-14 09:58:04 +0000

    completed get request for a postcode using httparty

---
+++ postcodesio/postcodesio_httparty.rb
@@ -0,0 +1,17 @@
+require 'httparty'
+require 'json'
+
+class Postcodesio
+  include HTTParty
+
+  base_uri 'https://api.postcodes.io'
+
+  def get_single_postcode(postcode)
+    JSON.parse(self.class.get("/postcodes/#{postcode}").body)
+  end
+
+end
+
+x = Postcodesio.new
+
+puts x.get_single_postcode('ha86lh')
